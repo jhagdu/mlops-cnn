abd0c256ee2185eecfabe17bb767ad194571e8b1
Aman Jhagrolia <amanjhagrolia143@gmail.com>
2020-05-22 09:21:28 +0530

    First

--- ml-cnn.py
+++ ml-cnn.py
@@ -1,104 +1,116 @@
-#from __future__ import print_function
-import keras
-#from keras.datasets import cifar100
-#from keras.preprocessing.image import ImageDataGenerator
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from keras.applications import vgg16
+from keras.optimizers import Adam
+import keras,os
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
-from keras.layers.normalization import BatchNormalization
-#from keras.regularizers import l2
-'''
-# Loads the CIFAR dataset
-(x_train, y_train), (x_test, y_test) = cifar100.load_data()
+from keras.layers import Dense, Conv2D, MaxPool2D , Flatten
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 
-# Display our data shape/dimensions
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
 
-# Now we one hot encode outputs
-num_classes = 100
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-...
+# In[2]:
 
-l2_reg = 0
 
-# Initialize model
+#model = vgg16.VGG16()
+
+
+# In[3]:
+
+
+trdata = ImageDataGenerator()
+traindata = trdata.flow_from_directory(directory="dataset/training_set/",target_size=(224,224))
+tsdata = ImageDataGenerator()
+testdata = tsdata.flow_from_directory(directory="dataset/testing_set/", target_size=(224,224))
+
+
+# In[4]:
+
+
 model = Sequential()
-
-# 1st Conv Layer 
-model.add(Conv2D(96, (11, 11), input_shape=x_train.shape[1:],
-    padding='same', kernel_regularizer=l2(l2_reg)))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-# 2nd Conv Layer 
-model.add(Conv2D(256, (5, 5), padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-'''
-# 3rd Conv Layer 
-model.add(ZeroPadding2D((1, 1)))
-model.add(Conv2D(512, (3, 3), padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-# 4th Conv Layer 
-model.add(ZeroPadding2D((1, 1)))
-model.add(Conv2D(1024, (3, 3), padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-
-# 5th Conv Layer 
-model.add(ZeroPadding2D((1, 1)))
-model.add(Conv2D(1024, (3, 3), padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-# 1st FC Layer
+model.add(Conv2D(input_shape=(224,224,3),filters=64,kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=64,kernel_size=(3,3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
 model.add(Flatten())
-model.add(Dense(3072))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-
-# 2nd FC Layer
-model.add(Dense(4096))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-'''
-# 3rd FC Layer
-model.add(Dense(num_classes))
-model.add(BatchNormalization())
-model.add(Activation('softmax'))
-
-print(model.summary())
-'''
-model.compile(loss = 'categorical_crossentropy',
-              optimizer = keras.optimizers.Adadelta(),
-              metrics = ['accuracy'])
+model.add(Dense(units=4096,activation="relu"))
+model.add(Dense(units=4096,activation="relu"))
+model.add(Dense(units=4, activation="softmax"))
 
 
+# In[5]:
 
-# Training Parameters
-batch_size = 32
-epochs = 1
 
-history = model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_data=(x_test, y_test),
-          shuffle=True)
+opt = Adam(lr=0.001)
+model.compile(optimizer=opt, loss=keras.losses.categorical_crossentropy, metrics=['accuracy'])
 
-model.save("CIFAR10_AlexNet_1_Epoch.h5")
 
-# Evaluate the performance of our trained model
-scores = model.evaluate(x_test, y_test, verbose=1)
-print('Test loss:', scores[0])
-print('Test accuracy:', scores[1])'''
+# In[6]:
+
+
+model.summary()
+
+
+# In[ ]:
+
+
+checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
+early = EarlyStopping(monitor='val_acc', min_delta=0, patience=20, verbose=1, mode='auto')
+
+hist = model.fit_generator(steps_per_epoch=100,generator=traindata, validation_data=testdata, validation_steps=10,epochs=100)
+
+
+# In[ ]:
+
+
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.applications.vgg16 import decode_predictions
+from keras.applications.vgg16 import VGG16
+# load the model
+#model = VGG16()
+# load an image from file
+image = load_img('./download.jpg', target_size=(224, 224))
+# convert the image pixels to a numpy array
+image = img_to_array(image)
+# reshape data for the model
+image = np.expand_dims(image, axis=0)
+#image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+# prepare the image for the VGG model
+image = preprocess_input(image)
+# predict the probability across all output classes
+yhat = model.predict(image)
+# convert the probabilities to class labels
+#label = decode_predictions(yhat)[0]
+# retrieve the most likely result, e.g. highest probability
+#label = label[0][0]
+# print the classification
+#print('%s (%.2f%%)' % (label[1], label[2]*100))
+print(yhat)
+
+
+# In[ ]:
+
+
+
+
