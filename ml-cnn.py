e0954476e0185db21c916c3535493195a0ea80b1
Aman Jhagrolia <amanjhagrolia143@gmail.com>
2020-05-22 16:34:42 +0530

    First

--- ml-cnn.py
+++ ml-cnn.py
@@ -1,72 +1,70 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
-from keras.preprocessing.image import ImageDataGenerator
+import sys
+import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
 from keras.layers.normalization import BatchNormalization
+from keras.preprocessing.image import ImageDataGenerator
 from keras.regularizers import l2
 from keras.datasets import mnist
 from keras.utils import np_utils
-import keras
+from Tuning_Model import set_kernel
 
-# loads the MNIST dataset
+# Loading MNIST Dataset
 (x_train, y_train), (x_test, y_test)  = mnist.load_data()
 
-# Lets store the number of rows and columns
-img_rows = x_train[0].shape[0]
-img_cols = x_train[1].shape[0]
+# Finding No. of Rows and Columns
+rows_of_img = x_train[0].shape[0]
+cols_of_img = x_train[1].shape[0]
 
 # Getting our date in the right 'shape' needed for Keras
 # We need to add a 4th dimenion to our date thereby changing our
 # Our original image shape of (60000,28,28) to (60000,28,28,1)
-x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+x_train = x_train.reshape(x_train.shape[0], rows_of_img, cols_of_img, 1)
+x_test = x_test.reshape(x_test.shape[0], rows_of_img, cols_of_img, 1)
 
 # store the shape of a single image 
-input_shape = (img_rows, img_cols, 1)
+input_shape = (rows_of_img, cols_of_img, 1)
 
 # change our image type to float32 data type
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 
-# Normalize our data by changing the range from (0 to 255) to (0 to 1)
+# Featuring Scaling - Normalization
 x_train /= 255
 x_test /= 255
 
-# Now we one hot encode outputs
+# Doing One-Hot Encoding
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 
-num_classes = y_test.shape[1]
-num_pixels = x_train.shape[1] * x_train.shape[2]
+n_classes = y_test.shape[1]
 
 
 # In[17]:
 
+# Set Kernel Size
+value = int(sys.argv[1])
+kernel_size = (value,value)
 
-#create model
+# Creating model
 model = Sequential()
 
-#2 sets of CRP 
-model.add(Conv2D(20,(5,5),padding="same",input_shape=input_shape))
+# Adding CRP layers
+model.add(Conv2D(20,kernel_size,padding="same",input_shape=input_shape))
 model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Conv2D(50,(5,5),padding="same"))
+model.add(Conv2D(50,kernel_size,padding="same"))
 model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
 
-#FC
+# FC
 model.add(Flatten())
 model.add(Dense(500))
 model.add(Activation("relu"))
 
-model.add(Dense(num_classes))
+model.add(Dense(n_classes))
 model.add(Activation("softmax"))
 
 model.compile(loss="categorical_crossentropy", optimizer=keras.optimizers.Adadelta(),metrics=['accuracy'])
@@ -99,7 +97,6 @@ print('Test accuracy:', scores[1])
 
 
 '''
-
 
 
 
