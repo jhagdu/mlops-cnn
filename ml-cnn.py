a4de18cf5e35bf28df78402c7133c3e35908d69a
Aman Jhagrolia <amanjhagrolia143@gmail.com>
2020-05-22 11:46:14 +0530

    First

--- ml-cnn.py
+++ ml-cnn.py
@@ -3,3 +3,6 @@
 from os import system
 accuracy = 10
 system("bash -c echo '{}' | cat > /root/1-Pull-Code/result".format(accuracy))
+file.open("result", "w+")
+file.write(str(accuracy))
+file(close)
